c6570614ef76176270d3da0063f71da205f6a31e
kulikov <kulikov.dm@gmail.com>
2012-11-28 09:42:43 +0400

    -

--- src/main/scala/sandbox/Test.scala
+++ src/main/scala/sandbox/Test.scala
@@ -1,14 +1,28 @@
 
 trait A {
- def receive: String
+  def receive: String
 }
+
 
 trait B extends A {
- abstract override def receive = super.receive + " updated!"
+  val myname = "Dima!"
+  abstract override def receive = super.receive + " updated!"
 }
+
 
 class My extends A {
- def receive = "My super 1"
+  this: B ⇒
+
+  def receive = "My super 1 " + myname
 }
 
-println((new My with B).receive)
+
+//println((new My with B).receive)
+
+
+
+case class Doc(id: Option[Int], n: Double = math.random)
+
+val docs = Map(Doc(Some(1)) → 12, Doc(Some(3)) → 13, Doc(Some(1)) → 22, Doc(Some(43)) → 1)
+
+println("\n | " + docs.filterNot(_._1.id == Some(1)) + "\n |\n")
